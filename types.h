af9bbd4cd2a52b7b41f7124e048446a0a721cc92
Kai Jie Chow <kj1chow@gmail.com>
2021-10-12 23:33:07 +0100

    tbc tmr

--- types.h
+++ types.h
@@ -1,18 +1,23 @@
 #include "imports.h"
 
 template <typename numbers = double>
-class SimpleVector
+class MatrixType
 {
 public:
-    // Default Constructor
-    SimpleVector(){};
-    // 1D Vector
+protected:
+};
+
+template <typename numbers = double>
+class VectorType
+{
+public:
     SimpleVector(vector<numbers> vector_coef, bool rowVector = true)
     {
         mainVector_ = vector_coef;
         vectorSize_ = (int)vector_coef.size();
         rowVector_ = rowVector;
     }
+
     void printOutline()
     {
         string line = "";
@@ -55,18 +60,8 @@ public:
     {
         return SimpleVector<numbers>(mainVector_, !rowVector_);
     }
-    // scalar multiplication
-    SimpleVector<numbers> operator*(numbers target)
-    {
-        vector<numbers> ans;
-        for (int i = 0; i < (int)mainVector_.size(); ++i)
-        {
-            ans.push_back(mainVector_[i] * target);
-        }
-        return SimpleVector(ans, rowVector_); // size remain the same
-    }
 
-private:
+protected:
     vector<numbers> mainVector_;
     unsigned int vectorSize_;
     bool rowVector_;
